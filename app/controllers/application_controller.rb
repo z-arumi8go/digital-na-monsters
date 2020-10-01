08fba19b8ed9696470a074b0dc0525ed0decf377
yamaura.wataru <yamaura.wataru@DonutsnoMacBook-Pro.local>
2020-10-01 17:58:05 +0900

    API関連下準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    
+    def authentication
+       if !token || !current_user
+        error "Authentication ERROR"
+       end
+    end
+
+    
+    def ok(data = nil;, message = nil)
+        render json: { status: 'SUCCESS', message: message, data: data }
+    end
+
+    def error(message = nil)
+        render json: { status: 'ERROR', message: message }
+    end
+
+
+    def token
+        @token ||= request.headers[:TOKEN]
+    end
+
+    def current_user
+        @current_user ||= Auth.find_by(token: token).user
+    end
 end
